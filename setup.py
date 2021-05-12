3576b97008bae28453a5ee61824ef03111dbee41
Luh <65669921+luyandadhlamini@users.noreply.github.com>
2021-05-12 16:23:41 +0200

    update setup.py
    Removed mimetypes from install requires since it is a standard library.

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@ from distutils.core import setup
 setup(
   name = 'googledrivepython',         
   packages = ['googledrivepython'],   
-  version = '0.1.4',      
+  version = '0.1.5',      
   license='MIT',        
   description = 'A python Google Drive API v3 wrapper',   
   author = 'Luyanda Dhlamini',                   
   author_email = 'luyanda.dhlamini@gmail.com',  
   url = 'https://github.com/luyandadhlamini',   
-  download_url = 'https://github.com/luyandadhlamini/googledrivepython/archive/refs/tags/v0.1.4.tar.gz', 
+  download_url = 'https://github.com/luyandadhlamini/googledrivepython/archive/refs/tags/v0.1.5.tar.gz', 
   keywords = ['Python', 'Google Drive', 'drive', 'API', 'wrapper' ],   
   install_requires=[            
           'oauth2-client',
-          'mimetypes',
           'google-api-python-client'
       ],
   classifiers=[
