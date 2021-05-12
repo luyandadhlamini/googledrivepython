1a1bed9c732dc8a30f30722ceb8d96246bb16ac8
Luh <65669921+luyandadhlamini@users.noreply.github.com>
2021-05-12 15:46:45 +0200

    Update setup.py added minor version change suffix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'googledrivepython',         
   packages = ['googledrivepython'],   
-  version = '0.1',      
+  version = '0.1.2',      
   license='MIT',        
   description = 'A python Google Drive API v3 wrapper',   
   author = 'Luyanda Dhlamini',                   
